94c728e0263f25ec0eb3e871ce5176493cff1aa2
Annikatheamazing <annikaarora.6316@gmail.com>
2025-04-22 17:59:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function take_snapshot()
 {
 
     Webcam.snap(function(data_uri)  {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src=""'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
 }
 
